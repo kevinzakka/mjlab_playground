42db1ff53c392c9a48ff01361d2ab1167a4002f6
Kevin Zakka <kevinarmandzakka@gmail.com>
2026-04-10 13:04:36 -0700

    Tweak rewards.

--- src/mjlab_playground/dexterous_tool/dexterous_tool_env_cfg.py
+++ src/mjlab_playground/dexterous_tool/dexterous_tool_env_cfg.py
@@ -175,7 +175,7 @@ def make_dexterous_tool_env_cfg() -> ManagerBasedRlEnvCfg:
         "fingertip_stds": (0.4, 0.1),
         "height_target": 0.6,
         "height_std": 0.1,
-        "pos_stds": (0.15, 0.05, 0.02),
+        "pos_stds": (0.3, 0.1, 0.03),
         "ori_stds": (math.radians(60.0), math.radians(20.0), math.radians(5.0)),
         "table_contact_sensor_name": "tool_table_collision",
       },
